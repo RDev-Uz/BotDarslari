81716e500f5fcbbe3cc9446701c168683bdeefa9
Rahmatulloh <100528967+RDev-Uz@users.noreply.github.com>
2023-01-22 15:38:09 +0500

    Update 2dars.py

--- code/2dars.py
+++ code/2dars.py
@@ -6,15 +6,10 @@ from aiogram import Bot, Dispatcher, executor, types
 logging.basicConfig(level=logging.INFO)
 
 # Bot obyekti
-bot = Bot(token="5877758914:AAHji8ugLFHJ7Co7rAj1P8BRq_PyPmLSgYQ")
+bot = Bot(token="API_TOKEN")
 # Bot uchun dispetcher
 dp = Dispatcher(bot)
 
-# Botga jo'natilgan /start buyrug'ini qabul qilib olish uchun handler
-@dp.message_handler(commands=['start'])
-async def start_cmd(message: types.Message):
-    # foydalanuvchiga javoban salom beradi
-    await message.answer("Assalomu alaykum!")
 
 @dp.message_handler(commands=['format'])
 async def formating(message: types.Message):
@@ -27,13 +22,7 @@ async def formating(message: types.Message):
     await message.answer(
         text="*Black*, _italic_, __underline__, ~strikethrough~, [link](https://example.com), ||spoiler||\n\nMarkdownV2",
         parse_mode='MarkdownV2')
-
-
-# Foydalanuvchidan kelgan matnni(textni) qabul qilib olish uchun handler
-@dp.message_handler(content_types=['text'])
-async def echo(message: types.Message):
-    # Foydalanuvchi jo'natgan matnga javoban jo'natgan matnini o'ziga yuboradi
-    await message.reply(message.text)
-
+    
+   
 if __name__ == '__main__':
     executor.start_polling(dp,skip_updates=True)
